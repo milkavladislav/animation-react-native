497da92d3bd85d3a41a95be9753b6d1ab76b7848
Milka Vladislav <76705901+milkavladislav@users.noreply.github.com>
2022-03-24 23:56:09 +0200

    do task

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from "react";
+import { Animated, Button, Dimensions, Easing, StatusBar} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const windowHeight = Dimensions.get('window').height;
+  const value = useRef(new Animated.Value(0)).current
+
+  const startAnimate = () => {
+    Animated.loop(
+      Animated.sequence([
+          Animated.timing(value, { toValue: windowHeight - 40, useNativeDriver: true, duration: 1000, easing: Easing.bounce, delay: 1000 }),
+          Animated.timing(value, { toValue: 0, useNativeDriver: true, duration: 1500, easing: Easing.cubic }),
+      ])
+  ).start()
+     }
+
+  return <Animated.View style={{ transform: [{ translateY: value}], marginTop: StatusBar.currentHeight, height: 40, justifyContent: 'center', backgroundColor: '#4649ad' }}  >
+      <Button title={'start animate'} onPress={startAnimate} />
+  </Animated.View>
+}
